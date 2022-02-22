431e102c6725488f427e22e0605472c8cab13e3d
Junxi He <junxihe459@gmail.com>
2022-02-22 11:53:45 -0500

    add: Create database model for "user"

--- ping/models.py
+++ ping/models.py
@@ -1,7 +1,18 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
 
-# Create your models here.
+# Test model
+# Ignore this one, it is only for testing purposes
 class GroupMember(models.Model):
     name = models.CharField(max_length=200)
     email = models.CharField(max_length=200)
+
+
+class User(models.Model):
+    id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=200, db_index=True)
+    password = models.CharField(max_length=200)
+    herbs = ArrayField(ArrayField(models.CharField(max_length=200), default=list, size=3), default=list, null=True)
+    spices = ArrayField(ArrayField(models.CharField(max_length=200), default=list, size=3), default=list, null=True)
+    proteins = ArrayField(ArrayField(models.CharField(max_length=200), default=list, size=3), default=list, null=True)
