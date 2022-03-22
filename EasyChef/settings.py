6417fd727c5eb6f33f94225ed17032be014f3768
Junxi He <junxihe459@gmail.com>
2022-03-21 22:48:10 -0400

    add: heroku set up

--- EasyChef/settings.py
+++ EasyChef/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -169,3 +171,5 @@ SWAGGER_SETTINGS = {
         }
     },
 }
+
+django_heroku.settings(locals())
