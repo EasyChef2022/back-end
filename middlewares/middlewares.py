ca17ec53cb72d7324b15e861faaf191ed0afb9c6
Junxi He <junxihe459@gmail.com>
2022-03-23 21:40:38 -0400

    fix: jwt should in http header

--- middlewares/middlewares.py
+++ middlewares/middlewares.py
@@ -16,22 +16,25 @@ class JWTMiddleware(MiddlewareMixin):
             return None
         else:
             body = json.loads(request.body)
-            if 'token' not in body:
-                response = {'success': 0, 'message': 'Log in first'}
+            header = request.headers
+            try:
+                token = header['Authorization'].split(' ')[1]
+            except:
+                response = {'success': 0, 'message': 'Invalid token'}
                 return HttpResponse(json.dumps(response), content_type="application/json", status=403)
-            else:
-                # Token authentication
-                try:
-                    payload = jwt_auth.decode_token(body['token'], JWT_secret)
-                except:
-                    response = {'success': 0, 'message': 'Invalid token'}
-                    return HttpResponse(json.dumps(response), content_type="application/json", status=403)
-                # Check if username are the same
-                if 'username' not in payload or payload['username'] != body['username']:
-                    response = {'success': 0, 'message': 'Invalid token and username'}
-                    return HttpResponse(json.dumps(response), content_type="application/json", status=403)
-                # Check if token is expired
-                if 'timestamp' not in payload or int(time.time()) - int(payload['timestamp']) > 60 * 60:
-                    response = {'success': 0, 'message': 'Token expired'}
-                    return HttpResponse(json.dumps(response), content_type="application/json", status=403)
-                return None
+
+            # Token authentication
+            try:
+                payload = jwt_auth.decode_token(token, JWT_secret)
+            except:
+                response = {'success': 0, 'message': 'Invalid token'}
+                return HttpResponse(json.dumps(response), content_type="application/json", status=403)
+            # Check if username are the same
+            if 'username' not in payload or payload['username'] != body['username']:
+                response = {'success': 0, 'message': 'Invalid token and username'}
+                return HttpResponse(json.dumps(response), content_type="application/json", status=403)
+            # Check if token is expired
+            if 'timestamp' not in payload or int(time.time()) - int(payload['timestamp']) > 60 * 60:
+                response = {'success': 0, 'message': 'Token expired'}
+                return HttpResponse(json.dumps(response), content_type="application/json", status=403)
+            return None
